0c8907056578bdbd0e269eb2108145bc404528d3
Randy Perez <randyjp125@gmail.com>
2016-04-19 18:20:10 -0400

    Initial Commit

---
+++ NPF-4/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  NPF-4
+//
+//  Created by Randy Perez on 4/19/16.
+//  Copyright © 2016 Randy Perez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
