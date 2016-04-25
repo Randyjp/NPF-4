10a90d7e9d04afd4870469193d7f8f53e5c22c4e
Randy Perez <randyjp125@gmail.com>
2016-04-25 19:23:54 -0400

    Done with map screen

--- NPF-4/MapVC.swift
+++ NPF-4/MapVC.swift
@@ -9,10 +9,17 @@
 import UIKit
 import CoreLocation
 import MapKit
+import Contacts
 
-class MapVC: UIViewController, MKMapViewDelegate {
+class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var activity: UIActivityIndicatorView!
+    @IBOutlet weak var btnRefresh: UIButton!
+    
+    var locationManager : CLLocationManager?
+    var startLocation: CLLocation!
+    
     var parkList = Parks()
     var parks : [Park] {
         get {
@@ -26,15 +33,70 @@ class MapVC: UIViewController, MKMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        
+        mapView?.addAnnotations(parks)
+        mapView?.delegate = self
+        
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager?.requestWhenInUseAuthorization()
+        activity.stopAnimating()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        //Optional method to handle error when getting user location.
+        let errorAlert = UIAlertController(title: "Error", message: "NP-4 was unable to get your location", preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        errorAlert.addAction(okAction)
+        self.presentViewController(errorAlert, animated: true, completion: nil)
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        //method to respond to auth changes by the user, like notify if we can't get the location and suggest changes.
+        switch status {
+        case .AuthorizedAlways, .AuthorizedWhenInUse:
+            locationManager?.startUpdatingLocation()
+        case .NotDetermined:
+            locationManager?.requestWhenInUseAuthorization()
+        case .Restricted, .Denied:
+            let alertController = UIAlertController(title: "Location Disbled", message: "NP-4 needs your location to function properly, please your permission settings to allow it", preferredStyle: .Alert)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            let openAction = UIAlertAction(title: "Open Settings", style: .Default, handler: {
+                (action) in if let url = NSURL(string:UIApplicationOpenSettingsURLString) {
+                    UIApplication.sharedApplication().openURL(url)
+                }
+            })
+            
+            alertController.addAction(cancelAction)
+            alertController.addAction(openAction)
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation: AnyObject = locations[0]
+        let mKCoordinateRegion = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, 20000, 20000)
+        mapView?.setRegion(mKCoordinateRegion, animated: true)
+        
+        //add Anotation
+        let point = MKPointAnnotation()
+        point.coordinate = newLocation.coordinate
+        point.title = "This is your current location"
+        point.subtitle = "Driving directions will part from here."
+        mapView?.addAnnotation(point)
+        
+        if startLocation == nil {
+            startLocation = newLocation as! CLLocation
+            locationManager?.stopUpdatingLocation()
+        }
+        activity.stopAnimating()
+    }
 
     /*
     // MARK: - Navigation
@@ -45,6 +107,24 @@ class MapVC: UIViewController, MKMapViewDelegate {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    //called once for each annotation created - if no view returned
+    //use defualt pin
+    func stopUpdating() {
+        locationManager?.stopUpdatingLocation()
+    }
+    
+    func startUpdating()  {
+        startLocation = nil
+        locationManager?.startUpdatingLocation()
+    }
+    
+    
+    //annotation methods
+    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+        let annotation = view.annotation
+        print("The tittle of the annotation is \(annotation!.title)")
+    }
     
     //called once for each annotation created - if no view returned
     //use defualt pin
@@ -78,5 +158,36 @@ class MapVC: UIViewController, MKMapViewDelegate {
         
         return nil
     }
+    
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        print("Control tapped: \(control), tag number= \(control.tag)")
+        let location = view.annotation as! Park
+        
+        if control.tag == 0 {
+            
+            let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+            location.mapItem().openInMapsWithLaunchOptions(launchOptions)
+        } else {
+            UIApplication.sharedApplication().openURL(NSURL(string: location.getLink())!)
+        }
+        
+        
+    }
+    
+    @IBAction func zoomOut(sender: AnyObject) {
+//        let address = [String(CNPostalAddressCountryKey):"USA"]
+//        let place = MKPlacemark(coordinate: CLLocationCoordinate2DMake(39.5, -98.35), addressDictionary: address)
+//        let mapItem = MKPlacemark(placemark: place)
+        
+        let mKCoordinateRegion = MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake(39.5, -98.35), 450000, 4500000)
+        mapView?.setRegion(mKCoordinateRegion, animated: true)
+    }
+    
+    @IBAction func refresh(sender: AnyObject) {
+        locationManager?.delegate = self
+        activity.startAnimating()
+        startUpdating()
+    }
+    
 
 }
